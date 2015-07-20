e2567e83c6142d9c846accf55bd39f2a25be89da
Catalin <catalin94v@gmail.com>
2015-07-20 01:34:12 +0100

    Fixing the images problem

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -4,9 +4,9 @@ app.controller('HomeController', function($scope) {
 	$scope.pageClass = "page-home";
 
 	$scope.slides = [
-		{image: '../../images/contacts.jpg', description: 'Image 00'},
-		{image: '../../images/bookmark.jpg', description: 'Image 00'},
-		{image: '../../images/todo.jpg', description: 'Image 00'}
+		{image: '../images/contacts.jpg', description: 'Image 00'},
+		{image: '../images/bookmark.jpg', description: 'Image 00'},
+		{image: '../images/todo.jpg', description: 'Image 00'}
 	];
 
 	$scope.currentIndex = 0;
